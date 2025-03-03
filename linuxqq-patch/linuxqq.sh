173e4b07bb4023bd7f38d782d6aa2f2b2ca3f3d0
CAB233 <yidaduizuoye@outlook.com>
2025-03-03 11:58:20 +0800

    chore: 统一使用空格缩进

--- linuxqq-patch/linuxqq.sh
+++ linuxqq-patch/linuxqq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d ~/.config/QQ/versions ]; then
-	find ~/.config/QQ/versions -name sharp-lib -type d -exec rm -r {} \; 2>/dev/null
+    find ~/.config/QQ/versions -name sharp-lib -type d -exec rm -r {} \; 2>/dev/null
     find ~/.config/QQ/versions -name libssh2.so.1 -type f -exec rm {} \; 2>/dev/null
 fi
 
@@ -10,8 +10,8 @@ rm -rf ~/.config/QQ/crash_files/*
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 
 if [[ -f "${XDG_CONFIG_HOME}/qq-flags.conf" ]]; then
-	mapfile -t QQ_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/qq-flags.conf")"
-	echo "User flags:" ${QQ_USER_FLAGS[@]}
+    mapfile -t QQ_USER_FLAGS <<<"$(grep -v '^#' "${XDG_CONFIG_HOME}/qq-flags.conf")"
+    echo "User flags:" ${QQ_USER_FLAGS[@]}
 fi
 
 # 适配 LiteLoaderQQNT
